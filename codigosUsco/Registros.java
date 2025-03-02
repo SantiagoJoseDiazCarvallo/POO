74d2de1fccbddee864377f3c5aa84682e415d7a4
SantiagoJoseDiazCarvallo <u20242227693@usco.edu.co>
2025-03-02 18:28:01 -0500

    Se actualiza un código

--- codigosUsco/Registros.java
+++ codigosUsco/Registros.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 
 public class Registros{
     public static void main(String[] args) throws Exception {
-        System.out.print("Por favor ingrese su usuario: ");
-        Scanner vis = new Scanner (System.in);
-        String registroNombre = vis.nextLine();
-        System.out.println("Bienvenido al software de programación, "+registroNombre+"!");
+        Scanner registroNombre = new Scanner (System.in, "UTF-8");//Permite reconocer letras como la ñ, y tíldes en la cadena de caracteres.
+        String nombre;
+        do {
+            System.out.print("Bienvenido! Por favor escriba su nombre: ");
+            nombre = registroNombre.nextLine().trim();//Permite que se ingrese múltiple caracteres.
+            if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ ]+")) {
+                System.out.println("Entrada no válida. Debe ingresar solo letras!");//Aún no acepta tíldes ni la letra ñ, cuidado!
+            }
+        } while (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ ]+"));//Se repetirá hasta que se ingrese una cadena de caracteres.
+        System.out.println("Bienvenido, "+nombre+"!");
+        registroNombre.close();
     }
 }
